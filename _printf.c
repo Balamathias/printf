3bd80ec41dde3d29ef3db5f5b38c10caa40d4d9a
Balamathias <balamathias05@gmail.com>
2023-10-17 18:58:22 -0700

    New commit

--- _printf.c
+++ _printf.c
@@ -1,61 +1,58 @@
 #include "main.h"
 
 /**
-  * _printf - prints a string
-  * @format: the format string
-  * Return: the number of characters printed
+  * _printf - prints a formatted output
+  * @format: string containing the format specifiers
+  *
+  * Return: number of characters printed
   */
-
 int _printf(const char *format, ...) {
-  int total_char_to_print = 0;
-  va_list list_of_args;
+    int total_char_to_print = 0;
+    va_list list_of_args;
 
-  if (format == NULL) {
-    return -1;
-  }
-  va_start(list_of_args, format);
-
-  while (*format) {
-    if (*format != '%') {
-      int result = write(1, format, 1);
-      if (result == -1) {
-      }
-      total_char_to_print++;
-    } else {
-      format++;
-      if (*format == '\0') {
-        break;
-      }
-      if (*format == '%') {
-        int result = write(1, format, 1);
-        if (result == -1) {
-        }
-        total_char_to_print++;
-      } else if (*format == 'c') {
-        char c = va_arg(list_of_args, int);
-        int result = write(1, &c , 1);
-        if (result == -1) {
-        }
-        total_char_to_print++; 
-      } else if (*format == 's') {
-        char *str = va_arg(list_of_args, char *);
-        int str_length = strlen(str);
-        int result = write(1, str, str_length);
-        if (result == -1) {
-        }
-        total_char_to_print += str_length;
-      } else if (*format == 'd' || *format == 'i') {
-        int num = va_arg(list_of_args, int);
-        char num_str[12];  // Assuming a maximum of 11 digits for an int
-        int num_length = sprintf(num_str, "%d", num);
-        int result = write(1, num_str, num_length);
-        if (result == -1) {
-        }
-        total_char_to_print += num_length;
-      }
+    if (format == NULL) {
+        return -1;
     }
-    format++;
-  }
+    va_start(list_of_args, format);
+
+    while (*format) {
+        if (*format != '%') {
+            int result = write(1, format, 1);
+            if (result == -1) {
+            }
+            total_char_to_print++;
+        } else {
+            format++;
+            if (*format == '\0') {
+                break;
+            }
+            if (*format == '%') {
+                int result = write(1, format, 1);
+                if (result == -1) {
+                }
+                total_char_to_print++;
+            } else if (*format == 'c') {
+                int character = va_arg(list_of_args, int);
+                char char_to_print = (char)character;
+                int result = write(1, &char_to_print, 1);
+                if (result == -1) {
+                }
+                total_char_to_print++;
+            } else if (*format == 's') {
+                char *str = va_arg(list_of_args, char *);
+                int str_length = strlen(str);
+                int result = write(1, str, str_length);
+                if (result == -1) {
+                }
+                total_char_to_print += str_length;
+            }
+        }
+        format++;
+    }
+
+    va_end(list_of_args);
+    return total_char_to_print;
+}
 
   va_end(list_of_args);
   return total_char_to_print;
--- main.h
+++ main.h
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
-
 
 int _printf(const char *format, ...);
 
